57866f46cdce2b833f7acd026cda97d408e05086
jakirdc1 <jakirdc1@gmail.com>
2017-10-24 19:29:42 +0600

    now practice

---
+++ Riad/src/riad/com/Student.java
@@ -0,0 +1,9 @@
+
+package riad.com;
+
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Pallabi is a area");
+    }
+}
